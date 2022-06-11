704725b558461e14295dee616bf82ab6cb8d4b4c
Badri <programmingwithbadri@outlook.com>
2022-06-11 13:18:30 +0530

    test: add additional test cases

--- src/calculator/__test__/add.test.js
+++ src/calculator/__test__/add.test.js
@@ -5,15 +5,15 @@ describe("Add numbers",  function(){
       expect(add('')).toEqual(0);
     });
 
-    it("should return the number itself when a single number is passed", () => {
+    it("should return the number itself when a single number string is passed", () => {
       expect(add('10')).toEqual(10);
     });
 
-    it("should return the sum of two numbers if two numbers are passed", () => {
+    it("should return the sum of two numbers if two numbers string are passed", () => {
       expect(add('1,2')).toEqual(3);
     });
     
-    it("should return the sum of an unknown amount of numbers", function(){
+    it("should return the sum of an unknown amount of numbers string", function(){
       const unknownAmountOfNumbers = Math.floor((Math.random() * 100));
       const numbersArray = Array(unknownAmountOfNumbers).fill().map(
         () => Math.round(Math.random() * unknownAmountOfNumbers)
@@ -25,11 +25,31 @@ describe("Add numbers",  function(){
       expect(add(unknownAmountOfNumbersArg)).toEqual(sumOfTheArray);
     });
     
-    it("should not allow negative numbers to be passed as arguments", () => {
+    it("should not allow negative numbers string to be passed as arguments", () => {
       expect(() => add("-1,2")).toThrow(new Error("negatives not allowed"));
     });
 
     it("should allow \\n in between the input number string", () => {
       expect(add("1\n2,3")).toEqual(6);
+    });
+
+    it("should support single delimiter of length 1 in the input number string", () => {
+      expect(add("//;\n1;2")).toEqual(3);
+    });
+
+    it("should support single delimiter of any length in the input number string", () => {
+      expect(add("//[***]\n1***2***3")).toEqual(6);
+    });
+
+    it("should support multiple delimiters of length 1 in the input number string", () => {
+      expect(add("//[*][%]\n1*2%3")).toEqual(6);
+    });
+
+    it("should support multiple delimiters of any length in the input number string", () => {
+      expect(add("//[*][%]\n1***2%%3")).toEqual(6);
+    });
+
+    it("should ignore the numbers bigger than 1000", () => {
+      expect(add("1,2,3000")).toEqual(3);
     });
 });
