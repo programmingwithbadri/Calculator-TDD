f09cfa14e1b88d7f59f1b06e5f95e07627f107e9
Badri <programmingwithbadri@outlook.com>
2022-06-11 14:04:25 +0530

    feat: implement the remaining cases related to add functionality

--- src/calculator/add.js
+++ src/calculator/add.js
@@ -1,10 +1,32 @@
+const parseDelimiters = (customDelimiter) => {
+  let delimiters = '|' + customDelimiter;
+
+  if (customDelimiter.indexOf('[') === 0) {
+    customDelimiter = customDelimiter.split('[').slice(1);
+    delimiters = customDelimiter.reduce(function (acc, d) {
+      return acc += '|' + d.substring(0, d.length - 1).replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, '\\$&');
+    }, '');
+  }
+
+  return delimiters;
+};
+
+const parseNumbers = (stringNumbers) => {
+  let delimiters =  '\n|,';
+
+  if (stringNumbers.indexOf('//') === 0) {
+    delimiters += parseDelimiters(stringNumbers.substring(2, stringNumbers.indexOf('\n')));
+    stringNumbers = stringNumbers.substring(stringNumbers.indexOf('\n'));
+  }
+
+  return stringNumbers.split(new RegExp('(' + delimiters + ')'));
+};
+
 const add = (stringNumbers) => {
-  const numbers = stringNumbers
-    .replace(/(\n)/gm, ',') 
-    .split(',')                     
-    .map(n => parseInt(n, 10)) 
+  const numbers = parseNumbers(stringNumbers);
 
   return numbers.reduce((acc, num) => {
+    num = Number(num);
     if (num < 0) {
       throw new Error('negatives not allowed');
     }
